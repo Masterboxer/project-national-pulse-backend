362db5bc880fbf0c0da196d6e6c174af7dd9691b
Masterboxer <sriharish74@gmail.com>
2025-12-22 01:17:14 +0530

    Added Search Functionality Route

--- routes/user.go
+++ routes/user.go
@@ -9,12 +9,14 @@ import (
 
 func CreateUserRoutes(db *sql.DB, router *mux.Router) *mux.Router {
 
+	router.HandleFunc("/users/search", handlers.SearchUsers(db)).Methods("GET")
 	router.HandleFunc("/users", handlers.GetUsers(db)).Methods("GET")
 	router.HandleFunc("/users/{id}", handlers.GetUserById(db)).Methods("GET")
-	router.HandleFunc("/users", handlers.CreateUser(db)).Methods("POST")
 	router.HandleFunc("/users/{id}", handlers.UpdateUser(db)).Methods("PUT")
 	router.HandleFunc("/users/{id}", handlers.DeleteUser(db)).Methods("DELETE")
-	router.HandleFunc("/users/search", handlers.SearchUsers(db)).Methods("GET")
+	router.HandleFunc("/users", handlers.CreateUser(db)).Methods("POST")
+
+	// Buddy routes
 	router.HandleFunc("/users/{user_id}/buddies", handlers.GetUserBuddies(db)).Methods("GET")
 	router.HandleFunc("/users/{user_id}/buddies", handlers.AddBuddy(db)).Methods("POST")
 	router.HandleFunc("/users/{user_id}/buddies/{buddy_id}", handlers.RemoveBuddy(db)).Methods("DELETE")
